0ec66c8dd2703b7b9bd358e2e9b251047fa997ce
RoM1 <73537345+romain772@users.noreply.github.com>
2021-11-24 13:53:15 +0100

    Add files via upload

--- src/components/IccID.js
+++ src/components/IccID.js
@@ -43,7 +43,7 @@ class IccID extends React.Component {
                     <h1>Corriger les numéros ICCID</h1>
                     <h2>Copier la liste de numéros ci-dessous et cliquer sur "Valider"</h2>
                     <form onSubmit={this.handleSubmit}>
-                        <textarea onClick={this.test} name="idlist" value={this.state.textAreaValue} onChange={this.handleChange} rows="15" cols="33"/>
+                        <textarea placeholder="Saisissez les numéros ICCID au format '8933890000000000000F'" onClick={this.test} name="idlist" value={this.state.textAreaValue} onChange={this.handleChange} rows="15" cols="33"/>
                         <button type="submit" className="button is-danger">Valider</button>
                     </form>
                     <table className="resulttable">
